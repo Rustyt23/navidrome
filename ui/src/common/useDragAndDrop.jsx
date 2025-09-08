bb6450050db5a4df9d25aa2f16f44d53fd8dca06
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2025-09-08 23:56:23 +0530

    fix drag-and-drop hook export

--- ui/src/common/useDragAndDrop.jsx
+++ ui/src/common/useDragAndDrop.jsx
@@ -1,22 +1,23 @@
 import { useDrag, useDrop } from 'react-dnd'
 
 const useDragAndDrop = (type, item, accepts, onDrop) => {
-    const [{ isDragging }, dragRef] = useDrag(() => ({
-        type,
-        item,
-        collect: (monitor) => ({ isDragging: !!monitor.isDragging() }),
-        options: { dropEffect: 'move' },
-    }))
+  const [{ isDragging }, dragRef] = useDrag(() => ({
+    type,
+    item,
+    collect: (monitor) => ({ isDragging: !!monitor.isDragging() }),
+    options: { dropEffect: 'move' },
+  }))
 
-    const [, dropRef] = useDrop(() => ({
-        accept: accepts,
-        drop: onDrop,
-    }))
+  const [, dropRef] = useDrop(() => ({
+    accept: accepts,
+    drop: onDrop,
+  }))
 
-    return {
-        dragDropRef: (node) => dragRef(dropRef(node)),
-        isDragging,
-    }
+  return {
+    dragDropRef: (node) => dragRef(dropRef(node)),
+    isDragging,
+  }
 }
 
 export default useDragAndDrop
+
