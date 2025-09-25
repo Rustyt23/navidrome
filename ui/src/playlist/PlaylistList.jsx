3caa1ce038cf14b1a142a9217eeb526368b21b8c
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2025-09-26 03:01:04 +0530

    feat(ui): add playlist created date column

--- ui/src/playlist/PlaylistList.jsx
+++ ui/src/playlist/PlaylistList.jsx
@@ -133,6 +133,7 @@ const PlaylistList = (props) => {
       updatedAt: isDesktop && (
         <DateField source="updatedAt" sortByOrder={'DESC'} />
       ),
+      createdAt: isDesktop && <DateField source="createdAt" showTime />, 
       public: !isXsmall && (
         <TogglePublicInput source="public" sortByOrder={'DESC'} />
       ),
