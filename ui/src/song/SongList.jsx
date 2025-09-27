4c114812b8e5069c08a70c7203b2dd78097cd408
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2025-09-28 00:30:10 +0530

    Ensure songs per-page value is numeric

--- ui/src/song/SongList.jsx
+++ ui/src/song/SongList.jsx
@@ -138,7 +138,7 @@ const SongList = (props) => {
   useResourceRefresh('song')
 
   const songs = useSelector((state) => state.admin.resources.song)
-  const perPage = songs?.list?.params?.perPage ?? 50
+  const perPage = Number(songs?.list?.params?.perPage) || 50
 
   const handleRowClick = useCallback((id, basePath, record) => {
       // Convert songs.data to an array if it's an object
