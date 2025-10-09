aa6836880ac8cafac4157bbef49f9bb1595178df
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2025-10-10 03:26:52 +0530

    Fix pagination dropdown to reflect current page size

--- ui/src/common/Pagination.jsx
+++ ui/src/common/Pagination.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { Pagination as RAPagination, useListContext } from 'react-admin'
 
 export const Pagination = (props) => {
+  const { rowsPerPage: _ignoredRowsPerPage, ...rest } = props
   const { perPage } = useListContext()
   const rowsPerPage = perPage ?? 50
 
   return (
     <RAPagination
       rowsPerPageOptions={[25, 50, 100, 200, 500]}
+      {...rest}
       rowsPerPage={rowsPerPage}
-      {...props}
     />
   )
 }
