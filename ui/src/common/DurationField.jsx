84fbf665d663db45f2c2e7e2abd928c96570b77f
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2025-10-26 05:31:16 +0530

    Allow duration field styling

--- ui/src/common/DurationField.jsx
+++ ui/src/common/DurationField.jsx
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types'
 import { formatDuration } from '../utils'
 import { useRecordContext } from 'react-admin'
 
-export const DurationField = ({ source, ...rest }) => {
+export const DurationField = ({ source, className, ...rest }) => {
   const record = useRecordContext(rest)
   try {
-    return <span>{formatDuration(record[source])}</span>
+    return <span className={className}>{formatDuration(record[source])}</span>
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log('Error in DurationField! Record:', record)
-    return <span>00:00</span>
+    return <span className={className}>00:00</span>
   }
 }
 
@@ -18,6 +18,7 @@ DurationField.propTypes = {
   label: PropTypes.string,
   record: PropTypes.object,
   source: PropTypes.string.isRequired,
+  className: PropTypes.string,
 }
 
 DurationField.defaultProps = {
