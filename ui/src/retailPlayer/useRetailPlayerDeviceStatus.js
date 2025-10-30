250c0b7d15e8111a127d5742182df3f85e3f1f1d
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2025-10-30 16:30:58 +0530

    Poll retail player status for now playing updates

--- ui/src/retailPlayer/useRetailPlayerDeviceStatus.js
+++ ui/src/retailPlayer/useRetailPlayerDeviceStatus.js
@@ -114,6 +114,8 @@ const mapChannelListResponse = (payload) =>
     })
     .filter(Boolean)
 
+const STATUS_REFRESH_INTERVAL_MS = 5000
+
 const mapStatusPayloadToDevice = (baseDevice, payload, channelList) => {
   if (!baseDevice) {
     return null
@@ -552,6 +554,29 @@ const useRetailPlayerDeviceStatus = (slugParam) => {
   const refresh = useCallback(() => {
     setRefreshIndex((previous) => previous + 1)
   }, [])
+
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return undefined
+    }
+
+    if (!isApiEnabled || devicesLoading) {
+      return undefined
+    }
+
+    const deviceId = normalizeValue(baseDevice?.apiId)
+    if (!deviceId) {
+      return undefined
+    }
+
+    const intervalId = window.setInterval(() => {
+      refresh()
+    }, STATUS_REFRESH_INTERVAL_MS)
+
+    return () => {
+      window.clearInterval(intervalId)
+    }
+  }, [baseDevice?.apiId, devicesLoading, isApiEnabled, refresh])
 
   useEffect(() => {
     if (!isApiEnabled) {
