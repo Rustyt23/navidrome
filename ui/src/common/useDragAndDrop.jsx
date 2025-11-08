0b5ea8f190ca46d01579a63c9535dc88e4f62b02
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2025-11-09 00:15:08 +0530

    Align sidebar drag preview with main view

--- ui/src/common/useDragAndDrop.jsx
+++ ui/src/common/useDragAndDrop.jsx
@@ -1,22 +1,40 @@
+import { useCallback } from 'react'
 import { useDrag, useDrop } from 'react-dnd'
 
 const useDragAndDrop = (type, item, accepts, onDrop) => {
-    const [{ isDragging }, dragRef] = useDrag(() => ({
-        type,
-        item,
-        collect: (monitor) => ({ isDragging: !!monitor.isDragging() }),
-        options: { dropEffect: 'move' },
-    }))
+  const [{ isDragging }, dragRef, previewRef] = useDrag(() => ({
+    type,
+    item,
+    collect: (monitor) => ({ isDragging: !!monitor.isDragging() }),
+    options: { dropEffect: 'move' },
+  }))
 
-    const [, dropRef] = useDrop(() => ({
-        accept: accepts,
-        drop: onDrop,
-    }))
+  const [, dropRef] = useDrop(() => ({
+    accept: accepts,
+    drop: onDrop,
+  }))
 
-    return {
-        dragDropRef: (node) => dragRef(dropRef(node)),
-        isDragging,
-    }
+  const dragDropRef = useCallback(
+    (node) => {
+      dragRef(dropRef(node))
+    },
+    [dragRef, dropRef],
+  )
+
+  const setDragPreview = useCallback(
+    (node, options) => {
+      if (typeof previewRef === 'function') {
+        previewRef(node, options)
+      }
+    },
+    [previewRef],
+  )
+
+  return {
+    dragDropRef,
+    isDragging,
+    setDragPreview,
+  }
 }
 
 export default useDragAndDrop
