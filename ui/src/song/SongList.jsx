9d6b2a6601ec6dbf6a3cf9bc57a17845062a0a92
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2025-11-17 01:20:20 +0530

    Restore desktop song list on mobile

--- ui/src/song/SongList.jsx
+++ ui/src/song/SongList.jsx
@@ -22,7 +22,6 @@ import {
   SongInfo,
   QuickFilter,
   SongTitleField,
-  SongSimpleList,
   RatingField,
   useResourceRefresh,
   ArtistLinkField,
@@ -133,7 +132,6 @@ const SongFilter = (props) => {
 const SongList = (props) => {
   const classes = useStyles()
   const dispatch = useDispatch()
-  const isXsmall = useMediaQuery((theme) => theme.breakpoints.down('xs'))
   const isDesktop = useMediaQuery((theme) => theme.breakpoints.up('md'))
   useResourceRefresh('song')
 
@@ -243,34 +241,30 @@ const SongList = (props) => {
         bulkActionButtons={<SongBulkActions />}
         actions={<SongListActions />}
         filters={<SongFilter />}
-        perPage={isXsmall ? 50 : 50}
+        perPage={50}
       >
-        {isXsmall ? (
-          <SongSimpleList />
-        ) : (
-          <SongDatagrid
-            rowClick={handleRowClick}
-            contextAlwaysVisible={!isDesktop}
-            classes={{ row: classes.row }}
-          >
-            <SongTitleField source="title" showTrackNumbers={false} />
-            {columns}
-            <SongContextMenu
-              source={'starred_at'}
-              sortByOrder={'DESC'}
-              sortable={config.enableFavourites}
-              className={classes.contextMenu}
-              label={
-                config.enableFavourites && (
-                  <FavoriteBorderIcon
-                    fontSize={'small'}
-                    className={classes.contextHeader}
-                  />
-                )
-              }
-            />
-          </SongDatagrid>
-        )}
+        <SongDatagrid
+          rowClick={handleRowClick}
+          contextAlwaysVisible={!isDesktop}
+          classes={{ row: classes.row }}
+        >
+          <SongTitleField source="title" showTrackNumbers={false} />
+          {columns}
+          <SongContextMenu
+            source={'starred_at'}
+            sortByOrder={'DESC'}
+            sortable={config.enableFavourites}
+            className={classes.contextMenu}
+            label={
+              config.enableFavourites && (
+                <FavoriteBorderIcon
+                  fontSize={'small'}
+                  className={classes.contextHeader}
+                />
+              )
+            }
+          />
+        </SongDatagrid>
       </List>
       <ExpandInfoDialog content={<SongInfo />} />
     </>
