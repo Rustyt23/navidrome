b1965ff93f88e3c734c6e53509c1d84900a2e2d2
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2025-11-29 22:13:47 +0530

    Expose retail player devices flag

--- server/serve_index.go
+++ server/serve_index.go
@@ -74,7 +74,7 @@ func serveIndex(ds model.DataStore, fs fs.FS, shareInfo *model.Share) http.Handl
 			"defaultDownsamplingFormat":  conf.Server.DefaultDownsamplingFormat,
 			"separator":                  string(os.PathSeparator),
 			"enableInspect":              conf.Server.Inspect.Enabled,
-			"retailPlayerDevicesEnabled": conf.Server.RetailPlayer.Enabled,
+			"retailPlayerDevicesEnabled": conf.Server.RetailPlayerDevicesEnabled,
 		}
 		if strings.HasPrefix(conf.Server.UILoginBackgroundURL, "/") {
 			appConfig["loginBackgroundURL"] = path.Join(conf.Server.BasePath, conf.Server.UILoginBackgroundURL)
