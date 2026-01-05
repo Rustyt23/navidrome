98de815d72c600920feb54ef301bd505e7109c15
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2026-01-05 23:28:20 +0530

    Purge missing synced playlists on refresh

--- scanner/refresh_playlists.go
+++ scanner/refresh_playlists.go
@@ -2,8 +2,11 @@ package scanner
 
 import (
 	"context"
+	"errors"
+	"os"
 	"sync/atomic"
 
+	"github.com/Masterminds/squirrel"
 	"github.com/navidrome/navidrome/core"
 	"github.com/navidrome/navidrome/core/artwork"
 	"github.com/navidrome/navidrome/model"
@@ -18,10 +21,39 @@ func RefreshPlaylists(ctx context.Context, ds model.DataStore, pls core.Playlist
 	defer release()
 
 	ctx = core.WithPlaylistRefreshPublic(ctx)
+	if err := purgeMissingSyncedPlaylists(ctx, ds); err != nil {
+		return err
+	}
 	state := scanState{
 		fullScan:        true,
 		changesDetected: atomic.Bool{},
 	}
 	phase := createPhasePlaylists(ctx, &state, ds, pls, artwork.NoopCacheWarmer())
 	return runPhase[*model.Folder](ctx, 4, phase)()
+}
+
+func purgeMissingSyncedPlaylists(ctx context.Context, ds model.DataStore) error {
+	playlists, err := ds.Playlist(ctx).GetAll(model.QueryOptions{
+		Filters: squirrel.And{
+			squirrel.Eq{"sync": true},
+		},
+	})
+	if err != nil {
+		return err
+	}
+	for _, pls := range playlists {
+		if pls.Path == "" {
+			continue
+		}
+		if _, err := os.Stat(pls.Path); err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				if err := ds.Playlist(ctx).Delete(pls.ID); err != nil {
+					return err
+				}
+				continue
+			}
+			return err
+		}
+	}
+	return nil
 }
