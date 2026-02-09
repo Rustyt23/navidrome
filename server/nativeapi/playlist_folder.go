41e17758ff426d296d708c8dbb8f56fd5c5c1ebc
Rustyt Rooser <61687579+Rustyt23@users.noreply.github.com>
2026-02-10 00:46:46 +0530

    Sort playlist folders and playlists alphabetically

--- server/nativeapi/playlist_folder.go
+++ server/nativeapi/playlist_folder.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -82,6 +83,13 @@ func ListFoldersAndPlaylists(ds model.DataStore) http.HandlerFunc {
 				Type:      "playlist",
 			})
 		}
+
+		sort.SliceStable(commons, func(i, j int) bool {
+			if commons[i].Type != commons[j].Type {
+				return commons[i].Type == "folder"
+			}
+			return strings.ToLower(commons[i].Name) < strings.ToLower(commons[j].Name)
+		})
 
 		total := len(commons)
 		start, end := opts.Offset, opts.Offset+opts.Max
